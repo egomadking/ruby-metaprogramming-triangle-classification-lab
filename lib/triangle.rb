c9f1dd71fa618a26f316e80deef58cde7bf05daa
Roy Mosby <ego.madking@gmail.com>
2020-12-24 10:28:31 -0500

    commit tree 97e2436e880a9a7ba5de40a7a6c4aa3afc68f955

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
   # write code here
+
+  attr_accessor :a, :b, :c
+
+  def initialize(a,b,c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    validate
+    if a == b && a == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def validate
+    if [a, b, c].any? { |i| i <= 0 }
+      raise TriangleError
+    end
+    if b + c <= a || a + c <= b || a + b <= c
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+    message = ""
+  end
 end
